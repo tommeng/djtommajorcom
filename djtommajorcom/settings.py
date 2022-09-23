a24700e7f70e85cf05157d761543f40521182fe8
Tom Meng <tommeng@spotify.com>
2022-09-23 17:38:14 -0400

    Removes db

--- djtommajorcom/settings.py
+++ djtommajorcom/settings.py
@@ -76,14 +76,6 @@ WSGI_APPLICATION = 'djtommajorcom.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': 5432,
-    }
 }
 
 
